22377284dd005eac39c48ecd0077b42e501bb7c5
Waleed1123 <waleed007896@gmail.com>
2023-05-14 23:02:42 +0500

    Test Preparation

---
+++ Test Preparation/app.js
@@ -0,0 +1,23 @@
+var cityToCheck = +prompt("Enter your city");
+cityToCheck = cityToCheck.toLowerCase();
+var cleanestCities = ["cheyenne", "santa fe", "tucson", "great falls", "honolulu"];
+for (var i = 0; i <= 4; i++) {
+if (cityToCheck === cleanestCities[i]) {
+alert("It's one of the cleanest cities");
+}
+}
+
+
+
+var input = document.getElementById("input");
+
+function inputName() {
+  console.log("Input: ", input.value );
+  
+};
+
+for (let i = 0; i < array.length; i++) {
+  
+  
+}
+
